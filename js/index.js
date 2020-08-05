045474830a2eada3ed73763c207d24b95a17d827
ummulmumeneena <ayishetuali.gh@gmail.com>
2020-08-05 17:53:05 +0000

    Used JS to  dynamically render the box

---
+++ js/index.js
@@ -0,0 +1,15 @@
+let colors=["red","blue","green","yellow", "brown", "ïndigo", "orange", "purple", "teal", "black"]
+function makeRow(){
+for(let index=0; index <colors,length; index++){
+    let newbox= document.createElement("div")
+    newbox.style.width="20%"
+    newbox.style.height="100%"
+    newbox.style.backgroundcolor=[index]
+    document.querySelector(".row").appendchild[newbox]
+}
+
+}
+makeRow()
+makeRow()
+makeRow()
+makeRow()
